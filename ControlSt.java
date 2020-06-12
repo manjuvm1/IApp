320a54a413d3ad09650db4185b74974b192cf714
manjuvm1 <65277114+manjuvm1@users.noreply.github.com>
2020-06-12 18:19:42 +0200

    Update ControlSt.java

--- ControlSt.java
+++ ControlSt.java
@@ -3,17 +3,37 @@ public class ControlSt {
         System.out.println("-----------While loop---------------");
         While();
         System.out.println("-----------DoWhile loop---------------");
-        DoWhile(10, 20);
+        DoWhile(10, 12);
+        System.out.println("-----------Switch control statement---------------");
+        Switch();
     }
 
-    private static void DoWhile(int a, int b) {
+    public static void Switch() {
+    	int i = 2;
+    	switch (i) {
+    	case 1: 
+    		System.out.println("Case 1");
+    		break;
+    	case 2: 
+    		System.out.println("Case 2");
+    		break;	
+    	case 10: 
+    		System.out.println("Case 1");
+    		break;
+    	default: 
+    		System.out.println("default");
+    	}
+    		
+    	}
+
+	public static void DoWhile(int a, int b) {
         do {
             System.out.println("Value of a : " + a);
             ++a;
-        } while (a > b);
+        } while (b > a);
     }
 
-    private static void While() {
+    public static void While() {
         int a = 10;
 
         while(a > 6) {
@@ -21,6 +41,4 @@ public class ControlSt {
             a--;
         }
     }
-
-
 }
